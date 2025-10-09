e3fc5dc3ad15e822d9b1fa00150d8da2a8c126cf
Sagnik Dutta <sagnikduttaofficial18@gmail.com>
2025-10-10 02:45:59 +0530

    New Changes

--- src/main/java/com/sagnik/NewBackend_Practice/Controller/StudentController.java
+++ src/main/java/com/sagnik/NewBackend_Practice/Controller/StudentController.java
@@ -1,4 +1,40 @@
 package com.sagnik.NewBackend_Practice.Controller;
 
+import com.sagnik.NewBackend_Practice.Model.Student;
+import com.sagnik.NewBackend_Practice.Service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/controller")
 public class StudentController {
+    @Autowired
+    private StudentService service;
+
+    @GetMapping
+    public List<Student> getAllStudents(){
+        return service.getAllStudents();
+    }
+    @GetMapping("/{id}")
+    public Student getStudent(@PathVariable int id){
+        return service.getStudentById(id);
+    }
+    @PostMapping
+    public String addStudent(@RequestBody Student student){
+        service.addStudent(student);
+        return "Student added Successfully";
+    }
+    @PutMapping("/{id}")
+    public String updateStudent(@PathVariable int id, @RequestBody Student student){
+        student.setId(id);
+        service.updateStudent(student);
+        return "Student Updated";
+    }
+    @DeleteMapping("/{id}")
+    public String deleteStudent(@PathVariable int id){
+        service.deleteStudent(id);
+        return "Student Deleted";
+    }
 }
