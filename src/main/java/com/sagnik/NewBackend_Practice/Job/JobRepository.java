241e47c45a4a04f5f3df55decd871248262a5d72
Sagnik Dutta <sagnikduttaofficial18@gmail.com>
2025-12-11 23:00:00 +0530

    Changes in Repository

--- src/main/java/com/sagnik/NewBackend_Practice/Job/JobRepository.java
+++ src/main/java/com/sagnik/NewBackend_Practice/Job/JobRepository.java
@@ -1,7 +1,9 @@
 package com.sagnik.NewBackend_Practice.Job;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface JobRepository extends JpaRepository<Job,Long>{
 
 }
--- src/main/java/com/sagnik/NewBackend_Practice/company/CompanyRepository.java
+++ src/main/java/com/sagnik/NewBackend_Practice/company/CompanyRepository.java
@@ -1,6 +1,8 @@
 package com.sagnik.NewBackend_Practice.company;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CompanyRepository extends JpaRepository<Company,Long> {
 }
