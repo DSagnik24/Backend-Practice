43007b69af9c5671eecef50acf57b07974c1eb17
Sagnik Dutta <sagnikduttaofficial18@gmail.com>
2025-12-17 20:50:56 +0530

    Added actuators and endpoints

--- src/main/java/com/sagnik/NewBackend_Practice/company/Company.java
+++ src/main/java/com/sagnik/NewBackend_Practice/company/Company.java
@@ -14,6 +14,7 @@ public class Company {
     private Long id;
     private String name;
     private String description;
+    private String rating;
 
     @JsonIgnore
     @OneToMany(mappedBy = "company")
--- src/main/java/com/sagnik/NewBackend_Practice/reviews/ReviewController.java
+++ src/main/java/com/sagnik/NewBackend_Practice/reviews/ReviewController.java
@@ -2,7 +2,6 @@ package com.sagnik.NewBackend_Practice.reviews;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
